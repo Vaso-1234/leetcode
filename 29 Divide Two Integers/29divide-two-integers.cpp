82a6bda9cae1a1af6bb1738b207fccce9b80da09
Vaishnavi Rohila <rohilavaishnavi29@gmail.com>
2025-10-25 16:10:37 +0000

    Code challenge submission for question: 29 Divide Two Integers

---
+++ 29 Divide Two Integers/29divide-two-integers.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int divide(int dividend, int divisor) {
+        if (dividend == INT_MIN && divisor == -1)
+            return INT_MAX;
+
+        bool negative = (dividend < 0) ^ (divisor < 0);
+
+        long long dvd = labs(dividend);
+        long long dvs = labs(divisor);
+        long long quotient = 0;
+
+        while (dvd >= dvs) {
+            long long temp = dvs, multiple = 1;
+
+            while ((temp << 1) <= dvd) {
+                temp <<= 1;
+                multiple <<= 1;
+            }
+
+            dvd -= temp;
+            quotient += multiple;
+        }
+
+        if (negative)
+            quotient = -quotient;
+
+        if (quotient > INT_MAX) return INT_MAX;
+        if (quotient < INT_MIN) return INT_MIN;
+
+        return (int)quotient;
+    }
+};
