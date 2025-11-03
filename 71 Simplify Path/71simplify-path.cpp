29efbd8a88e19d301d9da9f743419953c5decd86
Vaishnavi Rohila <rohilavaishnavi29@gmail.com>
2025-11-03 16:44:47 +0000

    Code challenge submission for question: 71 Simplify Path

---
+++ 71 Simplify Path/71simplify-path.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    string simplifyPath(string path) {
+        vector<string> stack;
+        string dir;
+        stringstream ss(path);
+       
+        while (getline(ss, dir, '/')) {
+            if (dir == "" || dir == ".") {
+                continue;
+            } 
+            else if (dir == "..") {
+                if (!stack.empty()) stack.pop_back();
+            } 
+            else {
+                stack.push_back(dir);
+            }
+        }
+
+        string result = "/";
+        for (int i = 0; i < stack.size(); i++) {
+            result += stack[i];
+            if (i != stack.size() - 1) result += "/";
+        }
+
+        return result;
+    }
+};
