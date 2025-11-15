5c3c252bc136e0f7d640f7f8e6935334798102b2
Vaishnavi Rohila <rohilavaishnavi29@gmail.com>
2025-11-15 18:33:24 +0000

    Code challenge submission for question: 99 Recover Binary Search Tree

---
+++ 99 Recover Binary Search Tree/99recover-binary-search-tree.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    TreeNode *first = NULL, *second = NULL, *prev = new TreeNode(INT_MIN);
+
+    void inorder(TreeNode* root) {
+        if (!root) return;
+
+        inorder(root->left);
+        if (prev->val > root->val) {
+            if (!first) first = prev;   
+            second = root;             
+        }
+
+        prev = root;
+        inorder(root->right);
+    }
+
+    void recoverTree(TreeNode* root) {
+        inorder(root);
+        swap(first->val, second->val);
+    }
+};
