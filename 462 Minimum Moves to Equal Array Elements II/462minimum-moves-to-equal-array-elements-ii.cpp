53d2d371cd4a26cb0172dd0882fb28206962645b
Vaishnavi Rohila <rohilavaishnavi29@gmail.com>
2025-12-01 17:31:14 +0000

    Code challenge submission for question: 462 Minimum Moves to Equal Array Elements II

---
+++ 462 Minimum Moves to Equal Array Elements II/462minimum-moves-to-equal-array-elements-ii.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int minMoves2(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        int n = nums.size();
+        int median = nums[n / 2];   
+        
+        long long moves = 0;
+        for (int x : nums)
+            moves += llabs((long long)x - median);
+        
+        return (int)moves;
+    }
+};
