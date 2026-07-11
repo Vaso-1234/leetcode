203898a7e6f97249a0ae1ba133d1d06812d74c79
Vaishnavi Rohila <rohilavaishnavi29@gmail.com>
2026-07-11 19:04:10 +0000

    Code challenge submission for question: 2685 Count the Number of Complete Components

---
+++ 2685 Count the Number of Complete Components/2685count-the-number-of-complete-components.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    vector<vector<int>> graph;
+    vector<bool> vis;
+
+    void dfs(int node, int &nodes, int &degreeSum) {
+        vis[node] = true;
+        nodes++;
+        degreeSum += graph[node].size();
+
+        for (int nei : graph[node]) {
+            if (!vis[nei])
+                dfs(nei, nodes, degreeSum);
+        }
+    }
+
+    int countCompleteComponents(int n, vector<vector<int>>& edges) {
+        graph.resize(n);
+        vis.assign(n, false);
+
+        for (auto &e : edges) {
+            graph[e[0]].push_back(e[1]);
+            graph[e[1]].push_back(e[0]);
+        }
+
+        int ans = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (!vis[i]) {
+                int nodes = 0;
+                int degreeSum = 0;
+
+                dfs(i, nodes, degreeSum);
+
+                int actualEdges = degreeSum / 2;
+                int requiredEdges = nodes * (nodes - 1) / 2;
+
+                if (actualEdges == requiredEdges)
+                    ans++;
+            }
+        }
+
+        return ans;
+    }
+};
