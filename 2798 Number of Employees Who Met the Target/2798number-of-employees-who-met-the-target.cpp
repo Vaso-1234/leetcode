bc79492c0e8d59564279a997b77755a51f40ef30
Vaishnavi Rohila <rohilavaishnavi29@gmail.com>
2026-07-27 17:53:07 +0000

    Code challenge submission for question: 2798 Number of Employees Who Met the Target

---
+++ 2798 Number of Employees Who Met the Target/2798number-of-employees-who-met-the-target.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int numberOfEmployeesWhoMetTarget(vector<int>& hours, int target) {
+        int c = 0;
+
+        for (int x : hours) {
+            if (x >= target)
+                c++;
+        }
+
+        return c;
+    }
+};
